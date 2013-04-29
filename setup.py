0ba4f9d7d8e55395a651ad69aaef2980a0342bd6
Q <q@q-continuum.net>
2013-04-29 13:03:59 -0600

    Moved libloader code into its own package and included some com loading code in the libloader.com module

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+from libloader import __author__, __doc__, __version__
+
+setup(
+ name = 'libloader',
+ version = __version__,
+ description = __doc__,
+ packages = find_packages(),
+ classifiers = [
+  'Development Status :: 3 - Alpha',
+  'Intended Audience :: Developers',
+  'Programming Language :: Python',
+  'Topic :: Software Development :: Libraries',
+ ],
+)
