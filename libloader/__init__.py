66b8b495c7cd6bf16d830e1286d6fbbc66568bc0
Christopher Toth <q.alpha@gmail.com>
2025-03-26 19:47:41 -0600

    Bump version

--- libloader/__init__.py
+++ libloader/__init__.py
@@ -1,8 +1,8 @@
 from .libloader import *
 
-__version__ = 0.21
+__version__ = "1.3.0"
 __author__ = "Christopher Toth"
 __author_email__ = "q@q-continuum.net"
 __doc__ = """
-Quickly and easily load shared libraries from various platforms. Also includes a libloader.com module for loading com modules on Windows.
+Quickly and easily load shared libraries on various platforms. Also includes a libloader.com module for loading com modules on Windows.
 """
